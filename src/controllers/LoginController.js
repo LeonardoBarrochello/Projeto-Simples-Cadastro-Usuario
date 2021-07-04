9d0c67e51c7530bcac44555f9b149fdc6b6c20e7
Leonardo <leonardobarrochello58@gmail.com>
2021-07-03 22:38:38 -0300

    carregando tela de usuario

--- src/controllers/LoginController.js
+++ src/controllers/LoginController.js
@@ -2,18 +2,16 @@ const Database =  require('../db/config')
 
 
 module.exports = {
-
         async open(req,res){
-            const db = await Database()
-            const email = req.body.email;
-            const pass = req.body.pass;
-            console.log(pass)
+           const db = await Database()
+           const email = req.body.email;
+           const pass = req.body.pass;
            const verifyUser =  await db.all(`SELECT * FROM usuario WHERE email = '${email}' `)
            var i = 0 ;
            var aux = 0 ;
            while(i<verifyUser.length){
                 if(verifyUser[i].pass === pass){
-                    res.redirect(`/home/?datalogged=true&&user=${verifyUser[i].id}`)
+                    res.render("logado",{logado:verifyUser[i].email})
                     aux = 1;
                     console.log(verifyUser[i])
                     break
--- src/route.js
+++ src/route.js
@@ -4,8 +4,8 @@ const route = express.Router()  //gerenciador de rotas do express
 const CadastroController = require('./controllers/CadastroController')
 const LoginController = require('./controllers/LoginController')
 
-route.get("/", (req,res) => res.render("home"))
-route.get("/home/?", (req,res) => res.render("home"))
+route.get("/", (req,res) => res.render("login"))
+route.get("/home", (req,res) => res.render("home"))
 route.get("/cadastro" , (req,res) => res.render("cadastro"))
 route.get("/login" , (req,res) => res.render("login"))
 
