bcd9e3de53037c8f2eda489910f25f83fabaee14
Leonardo <leonardobarrochello58@gmail.com>
2021-07-04 14:44:33 -0300

    connect-redis

--- src/controllers/LoginController.js
+++ src/controllers/LoginController.js
@@ -1,31 +1,24 @@
 const Database =  require('../db/config')
 const express = require('express')
 const session = require('express-session')
+const { OPEN_READWRITE } = require('sqlite3')
 const app = express()
 
 app.use(session({secret:'wswswswdfweweeewewe'}))
 
 module.exports = {
         async open(req,res){
-        
+           
            const db = await Database()
            const email = req.body.email;
            const pass = req.body.pass;
-           const verifyUser =  await db.all(`SELECT * FROM usuario WHERE email = '${email}' `)
-           var i = 0 ;
-           var aux = 0 ;
-           while(i<verifyUser.length){
-                if(verifyUser[i].pass === pass){
-                    
-                    res.render("logado",{logado:verifyUser[i].email}) //ver pq o session n funciona
-                    aux = 1;
-                    console.log(verifyUser[i])
-                    break
-                }
-                i++
+           const verifyUser =  await db.get(`SELECT * FROM usuario WHERE email = '${email}' and pass = ${pass} `)
+           if(!verifyUser == ''){
+                    req.session.login = verifyUser.email
+                    res.redirect("/painel/") //ver pq o session n funciona
+           }else{
+                res.render("parts/passincorrect")
            }
-           if(aux == 0){
-               res.render("parts/passincorrect")
-           }
+        
         }
 }
--- src/route.js
+++ src/route.js
@@ -1,16 +1,24 @@
 const express = require('express')
 const { resolveInclude } = require('ejs')
 const route = express.Router()  //gerenciador de rotas do express
+const session = require('express-session')
 const CadastroController = require('./controllers/CadastroController')
 const LoginController = require('./controllers/LoginController')
 
-route.get("/", (req,res) => res.render("login"))
-route.get("/home", (req,res) => res.render("home"))
+route.use(session({
+    secret:'key'
+}))
+
+route.get("/", (req,res) => {res.render("login")})
+route.get("/home", (req,res) => res.render("index"))
 route.get("/cadastro" , (req,res) => res.render("cadastro"))
 route.get("/login" , (req,res) => res.render("login"))
 
 route.post("/cadastro-conta",CadastroController.create)
-route.post("/login-conta",LoginController.open)
+route.post("/login-conta",(req,res)=> {
+    LoginController.open(req,res)
+
+})
 
 
 module.exports = route
