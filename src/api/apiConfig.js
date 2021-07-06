bc4c3d8de40bfc29f833f73f3958689576678b06
Leonardo <leonardobarrochello58@gmail.com>
2021-07-05 22:12:41 -0300

    upload image to api option added

--- src/api/apiConfig.js
+++ src/api/apiConfig.js
@@ -1,13 +1,33 @@
 const express = require('express');
 const routeApi = express.Router();
+
 const multer = require('multer')
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'uploads/')
+    },
+    filename: function (req, file, cb) {
+        // Extração da extensão do arquivo original:
+        const extensaoArquivo = file.originalname.split('.')[1];
+
+        // Cria um código randômico que será o nome do arquivo
+        const novoNomeArquivo = require('crypto')
+            .randomBytes(64)
+            .toString('hex');
+
+        // Indica o novo nome do arquivo:
+        cb(null, `${novoNomeArquivo}.${extensaoArquivo}`)
+    }
+});
+const upload = multer({ storage });
+
 
 routeApi.use(express.urlencoded({extended:true}))
 routeApi.get('/formtest',(req,res) => res.render('formTest'))
-routeApi.post('/cadastro' , (req,res) =>{
+routeApi.post('/cadastro' ,upload.single('foto'), (req,res) =>{
     const { nome, site } = req.body;
     res.json({nome,site})
 })
-
 
 module.exports = routeApi
